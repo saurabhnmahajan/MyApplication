9f54c3b166156fcf4bded7df7fbfd9ae4a881adb
Saurabh Mahajan <samahajan@paypal.com>
2019-08-06 22:23:51 -0700

    added fragment

---
+++ app/src/main/java/com/example/myapplication/BlankFragment.java
@@ -0,0 +1,56 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class BlankFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public BlankFragment() {
+        Log.d("recycler", "fragment created");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        ((TextView)view.findViewById(R.id.frag_text)).setText("Testing2");
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -21,46 +23,85 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        MyAdapter adapter = new MyAdapter(this, new String[] {"1", "2", "3", "4", "5", "6", "7", "8","9", "10", "11", "12", "2", "3", "4", "5", "6", "7", "8","9", "10", "11", "12"});
+        MyAdapter adapter = new MyAdapter(this, new String[] {"1", "2", "3", "4", "5", "6", "7", "8","9", "10", "11", "12", "2", "3", "4", "5", "6", "7", "8","9", "10", "11", "12"}, getSupportFragmentManager());
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
         recyclerView.setNestedScrollingEnabled(false);
     }
 
-    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         private String[] mDataset;
         private Context context;
+        private FragmentManager mFragmentManager;
 
         public class MyViewHolder extends RecyclerView.ViewHolder {
 
             public TextView textView;
+
             public MyViewHolder(View v) {
                 super(v);
                 textView = v.findViewById(R.id.item_text);
             }
         }
 
-        public MyAdapter(Context context, String[] myDataset) {
+        class ViewHolder2 extends RecyclerView.ViewHolder {
+
+            public ViewHolder2(@NonNull View itemView, FragmentManager fragmentManager) {
+                super(itemView);
+                fragmentManager.beginTransaction()
+                        .replace(itemView.getId(), new BlankFragment())
+                        .commit();
+
+                setIsRecyclable(false);
+            }
+        }
+
+        public MyAdapter(Context context, String[] myDataset, FragmentManager fragmentManager) {
             mDataset = myDataset;
             this.context = context;
+            mFragmentManager = fragmentManager;
         }
 
         @Override
-        public MyViewHolder onCreateViewHolder(ViewGroup parent,
-                                                         int viewType) {
-            View view =  LayoutInflater.from(context).inflate(R.layout.item, parent, false);
-            return new MyViewHolder(view);
+        public void onViewAttachedToWindow(@NonNull RecyclerView.ViewHolder holder) {
+            if(holder instanceof ViewHolder2) {
+                holder.setIsRecyclable(false);
+            }
+            super.onViewAttachedToWindow(holder);
         }
 
         @Override
-        public void onBindViewHolder(MyViewHolder holder, int position) {
-            holder.textView.setText(mDataset[position]);
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                          int viewType) {
+            if(viewType == 1) {
+                View view =  LayoutInflater.from(context).inflate(R.layout.item, parent, false);
+                return new MyViewHolder(view);
+            } else {
+                View view =  LayoutInflater.from(context).inflate(R.layout.fragment_blank, parent, false);
+                return new ViewHolder2(view, mFragmentManager);
+            }
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            if(holder instanceof MyViewHolder) {
+                ((MyViewHolder)holder).textView.setText(mDataset[position - 1]);
+
+            } else {
+                Log.d("recycler", "fragment");
+            }
             Log.d("recycler", position + "");
         }
 
         @Override
+        public int getItemViewType(int position) {
+            if(position == 0) return 0;
+            else return 1;
+        }
+
+        @Override
         public int getItemCount() {
-            return mDataset.length;
+            return mDataset.length + 1;
         }
     }
 }
